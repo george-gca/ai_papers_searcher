725c5c1a41989c5e646410814927703d614138be
George Araújo <george.gcac@gmail.com>
2024-07-27 14:48:59 -0300

    Added source_url field to PaperInfo
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- paperinfo.py
+++ paperinfo.py
@@ -4,12 +4,14 @@ from dataclasses import dataclass, field
 @dataclass
 class PaperInfo:
     abstract_url: str
-    pdf_url: str
-    title: str
     clean_title: str
+    pdf_url: str
+    source_url: int
+    title: str
     # indicates the weight of the word in this paper, being
     # key: index of word
     # value: weight of the word, associated with occurrence in title and/or abstract
     abstract_freq: dict[int, float] = field(default_factory=dict)
+    arxiv_id: None | str = None
     conference: str = ''
     year: int = 0
